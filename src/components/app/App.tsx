b005ff65a66937a3a0fd786212618954850f22ca
asosunoff <al.sosunoff@gmail.com>
2021-01-27 23:10:18 +0300

    fix app

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="container mt-3">
+      <div className="row justify-content-center">
+        <div className="col-4">
+          <button type="button" className="btn btn-primary">
+            Fetch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
