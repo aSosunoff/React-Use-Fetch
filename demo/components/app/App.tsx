a6849e0b18287f50f50aed242a3004686688ce15
asosunoff <al.sosunoff@gmail.com>
2021-02-02 19:45:31 +0300

    added eslint

--- demo/components/app/App.tsx
+++ demo/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useFetch } from "../../../src";
 /* import { useFetch } from "../../../dist"; */
 import ErrorImg from "../error-img";
@@ -16,18 +16,38 @@ const App: React.FC = () => {
     "https://jsonplaceholder.typicode.com/posts"
   );
 
+  const doFetchWrapper = useCallback(() => {
+    setClear(false);
+    doFetch();
+  }, [doFetch]);
+
+  const [isClear, setClear] = useState(false);
+  const clearHandler = useCallback(() => setClear(true), []);
+
+  const list = useMemo(() => (isClear ? [] : data), [data, isClear]);
+
   return (
     <div className="container">
       <div className="row mt-3">
         <div className="offset-md-4 col-4 d-flex justify-content-center">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={doFetch}
-            disabled={status === "request"}
-          >
-            Fetch
-          </button>
+          <div className="btn-group" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={doFetchWrapper}
+              disabled={status === "request"}
+            >
+              Fetch
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={clearHandler}
+              disabled={status === "request"}
+            >
+              Clear
+            </button>
+          </div>
         </div>
       </div>
 
@@ -43,7 +63,7 @@ const App: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {data?.map(({ userId, id, title, body }) => (
+              {list?.map(({ userId, id, title, body }) => (
                 <tr key={id}>
                   <th scope="row">{userId}</th>
                   <td>{id}</td>
